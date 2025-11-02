fe115b2f3e589ecba2dacab1f0e29c4fe74dfd3e
Tinalexman <taiwoluwatobilobafestus@gmail.com>
2025-11-02 19:29:56 +0100

    Ochimm

--- lib/core/ui/themes.dart
+++ lib/core/ui/themes.dart
@@ -1,6 +1,7 @@
 import 'package:codeathon/core/ui/colors.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 ThemeData lightThemeData = FlexThemeData.light(
   fontFamily: "PlusJakartaSans",
@@ -15,32 +16,28 @@ ThemeData lightThemeData = FlexThemeData.light(
 );
 
 class AppTextStyles {
-  static TextStyle get bold => TextStyle(
-    fontFamily: "PlusJakartaSans",
+  static TextStyle get bold => GoogleFonts.outfit(
     color: AppColors.dark,
     fontWeight: FontWeight.w700,
     height: 1.5,
     letterSpacing: -0.03,
   );
 
-  static TextStyle get semiBold => TextStyle(
-    fontFamily: "PlusJakartaSans",
+  static TextStyle get semiBold => GoogleFonts.outfit(
     color: AppColors.dark,
     fontWeight: FontWeight.w600,
     height: 1.5,
     letterSpacing: -0.02,
   );
 
-  static TextStyle get medium => TextStyle(
-    fontFamily: "PlusJakartaSans",
+  static TextStyle get medium => GoogleFonts.outfit(
     color: AppColors.grey,
     fontWeight: FontWeight.w500,
     height: 1.5,
     letterSpacing: -0.02,
   );
 
-  static TextStyle get regular => TextStyle(
-    fontFamily: "PlusJakartaSans",
+  static TextStyle get regular => GoogleFonts.outfit(
     color: AppColors.grey,
     fontWeight: FontWeight.w400,
     height: 1.5,
